397ab852d680f78720412418b1af9a70ed0f72c7
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2024-09-17 23:46:05 +0200

    Notes-App Created

---
+++ Notes-App/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_CvCto4AAwnNkyV-ZxMXc3uAma3hPZGQ",
+  authDomain: "notes-app-49202.firebaseapp.com",
+  projectId: "notes-app-49202",
+  storageBucket: "notes-app-49202.appspot.com",
+  messagingSenderId: "471218850432",
+  appId: "1:471218850432:web:5e89a4340f23463374f465"
+};
+
+const app = initializeApp(firebaseConfig);
